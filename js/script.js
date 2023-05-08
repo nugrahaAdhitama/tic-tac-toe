cda0d7b446ed2860630ef1d542e8a0c695ea44ea
nugrahaAdhitama <78828744+nugrahaAdhitama@users.noreply.github.com>
2023-05-08 18:42:33 +0700

    Refactor Tic Tac Toe app for better code organization and readability
    - Group related functions and variables in a single object (ticTacToe)
    - Use clean code practices to make functions smaller and focused on a single task
    - Rename variables and functions to be more descriptive
    - Add comments to explain complex parts of the code

--- js/script.js
+++ js/script.js
@@ -1,79 +1,92 @@
-let gameBoard = ["", "", "", "", "", "", "", "", ""];
-let currentPlayer = "X";
-let player1Score = 0;
-let player2Score = 0;
+const ticTacToe = {
+    gameBoard: ["", "", "", "", "", "", "", "", ""],
+    currentPlayer: "X",
+    player1Score: 0,
+    player2Score: 0,
+    cells: document.querySelectorAll(".cell"),
 
-const cells = document.querySelectorAll(".cell");
-cells.forEach((cell) => {
-    cell.addEventListener("click", handleCellClick);
-})
-
-const resetButton = document.getElementById("reset");
-resetButton.addEventListener("click", handleResetClick);
-
-function handleCellClick(event) {
-    const clickedCell = event.target;
-    const cellIndex = parseInt(clickedCell.getAttribute("data-index"));
-
-    if (gameBoard[cellIndex] !== "" || checkWinner()) {
-        return;
-    }
-
-    gameBoard[cellIndex] = currentPlayer;
-    clickedCell.textContent = currentPlayer;
-
-    if (checkWinner()) {
-        alert(`Player ${currentPlayer} wins!`);
-        if (currentPlayer === "X") {
-            player1Score++;
+    handleCellClick(event) {
+        const clickedCell = event.target;
+        const cellIndex = parseInt(clickedCell.getAttribute("data-index"));
+    
+        if (gameBoard[cellIndex] !== "" || checkWinner()) {
+            return;
+        }
+    
+        gameBoard[cellIndex] = currentPlayer;
+        clickedCell.textContent = currentPlayer;
+    
+        if (checkWinner()) {
+            alert(`Player ${currentPlayer} wins!`);
+            if (currentPlayer === "X") {
+                player1Score++;
+            } else {
+                player2Score++;
+            }
+            updateScore();
         } else {
-            player2Score++;
+            currentPlayer = currentPlayer === "X" ? "O" : "X";
         }
-        updateScore();
-    } else {
-        currentPlayer = currentPlayer === "X" ? "O" : "X";
-    }
-}
+    },
 
-function checkWinner() {
-    const winningCombinations = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
+    checkWinner() {
+        return this.checkRows() || this.checkColumns() || this.checkDiagonals();
+    },
 
-    for (const combination of winningCombinations) {
-        const [a, b, c] = combination;
-
-        if (
-            gameBoard[a] !== "" &&
-            gameBoard[a] === gameBoard[b] &&
-            gameBoard[a] === gameBoard[c]
-        ) {
+    checkRows() {
+        for (let i = 0; i < 9; i += 3) {
+            if (this.checkThreeCells(i, i + 1, i + 2)) {
             return true;
+            }
         }
-    }
+        return false;
+    },
 
-    return false;
+    checkColumns() {
+        for (let i = 0; i < 3; i++) {
+            if (this.checkThreeCells(i, i + 3, i + 6)) {
+            return true;
+            }
+        }
+        return false;
+    },
+
+    checkDiagonals() {
+      return this.checkThreeCells(0,4,8) || this.checkThreeCells(2,4,6);
+    },
+
+    checkThreeCells(a, b, c) {
+        return (
+            this.gameBoard[a] !== "" &&
+            this.gameBoard[a] === this.gameBoard[b] && 
+            this.gameBoard[a] === this.gameBoard[c]
+        )
+    },
+
+    updateScore() {
+        const player1ScoreDisplay = document.getElementById("player1");
+        const player2ScoreDisplay = document.getElementById("player2");
+    
+        player1ScoreDisplay.textContent = `Player 1: ${player1Score}`;
+        player2ScoreDisplay.textContent = `Player 2: ${player2Score}`;
+    },
+
+    handleResetClick() {
+        gameBoard = ["", "", "", "", "", "", "", "", ""];
+        currentPlayer = "X";
+        cells.forEach((cell) => {
+            cell.textContent = "";
+        })
+    },
+
+    init() {
+        this.cells.forEach((cell) => {
+            cell.addEventListener("click", this.handleCellClick);
+        })
+
+        const resetButton = document.getElementById("reset");
+        resetButton.addEventListener("click", this.handleResetClick);
+    },
 }
 
-function updateScore() {
-    const player1ScoreDisplay = document.getElementById("player1");
-    const player2ScoreDisplay = document.getElementById("player2");
-
-    player1ScoreDisplay.textContent = `Player 1: ${player1Score}`;
-    player2ScoreDisplay.textContent = `Player 2: ${player2Score}`;
-}
-
-function handleResetClick() {
-    gameBoard = ["", "", "", "", "", "", "", "", ""];
-    currentPlayer = "X";
-    cells.forEach((cell) => {
-        cell.textContent = "";
-    })
-}
+ticTacToe.init();
